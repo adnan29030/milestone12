d870f402cf7fe0c7092bb48d994ea66522cb2904
adnan29030 <smg29030@gmail.com>
2024-11-19 02:14:14 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+var ref_sec = document.getElementById("refrence");
+var ref_btn = document.getElementById("ref_btn");
+ref_btn === null || ref_btn === void 0 ? void 0 : ref_btn.addEventListener('click', function () {
+    ref_sec.classList.add('hide');
+});
+var contact_sec = document.getElementById("cont");
+var contact_btn = document.getElementById("contact");
+contact_btn === null || contact_btn === void 0 ? void 0 : contact_btn.addEventListener('click', function () {
+    contact_sec.classList.add('hide');
+});
+var education_sec = document.getElementById("edu");
+var edu_btn = document.getElementById("education");
+edu_btn === null || edu_btn === void 0 ? void 0 : edu_btn.addEventListener('click', function () {
+    education_sec.classList.add('hide');
+});
+var skill_sec = document.getElementById("exper");
+var skill_btn = document.getElementById("experties");
+skill_btn === null || skill_btn === void 0 ? void 0 : skill_btn.addEventListener('click', function () {
+    skill_sec.classList.add('hide');
+});
+var languge_sec = document.getElementById("langa");
+var langa_btn = document.getElementById("languge");
+langa_btn === null || langa_btn === void 0 ? void 0 : langa_btn.addEventListener('click', function () {
+    languge_sec.classList.add('hide');
+});
+var expi_sec = document.getElementById("expi");
+var expi_btn = document.getElementById("expirance");
+expi_btn === null || expi_btn === void 0 ? void 0 : expi_btn.addEventListener('click', function () {
+    expi_sec.classList.add('hide');
+});
---
+++ index.ts
@@ -0,0 +1,51 @@
+let ref_sec=document.getElementById("refrence")
+let ref_btn=document.getElementById("ref_btn")
+
+
+ref_btn?.addEventListener('click',()=>{
+    ref_sec!.classList.add('hide')
+})
+
+
+let contact_sec=document.getElementById("cont")
+let contact_btn=document.getElementById("contact")
+
+
+contact_btn?.addEventListener('click',()=>{
+    contact_sec!.classList.add('hide')
+})
+
+
+let education_sec=document.getElementById("edu")
+let edu_btn=document.getElementById("education")
+
+
+edu_btn?.addEventListener('click',()=>{
+education_sec!.classList.add('hide')
+})
+
+
+let skill_sec=document.getElementById("exper")
+let skill_btn=document.getElementById("experties")
+
+
+skill_btn?.addEventListener('click',()=>{
+    skill_sec!.classList.add('hide')
+})
+
+
+let languge_sec=document.getElementById("langa")
+let langa_btn=document.getElementById("languge")
+
+
+langa_btn?.addEventListener('click',()=>{
+    languge_sec!.classList.add('hide')
+})
+
+let expi_sec=document.getElementById("expi")
+let expi_btn=document.getElementById("expirance")
+
+
+expi_btn?.addEventListener('click',()=>{
+    expi_sec!.classList.add('hide')
+})
